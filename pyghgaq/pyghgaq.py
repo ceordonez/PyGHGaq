9939951a2193865281023d978d7ba06c726fb90d
César Ordóñez <ceordonezv@gmail.com>
2025-10-29 13:02:41 +0100

    implementing registry for henry's coefficientes

--- pyghgaq/pyghgaq.py
+++ pyghgaq/pyghgaq.py
@@ -1,10 +1,12 @@
 import numpy as np
-import pandas as pd
+from functions import *
 
 
 def csat(
-    atmpress_hpa: np.ndarray, catm_ppm: np.ndarray, hcp_molm3pa: np.ndarray
-) -> np.ndarray:
+    atmpress_hpa: np.ndarray | float,
+    catm_ppm: np.ndarray | float,
+    hcp_molm3pa: np.ndarray | float,
+) -> np.ndarray | float:
     """
     Return
     -------
@@ -14,8 +16,10 @@ def csat(
 
 
 def atm_diff_flux(
-    csat_mmolm3: np.ndarray, cw_mmolm3: np.ndarray, k_md: np.ndarray
-) -> np.ndarray:
+    csat_mmolm3: np.ndarray | float,
+    cw_mmolm3: np.ndarray | float,
+    k_md: np.ndarray | float,
+) -> np.ndarray | float:
     """
     Returns
     -------
@@ -24,57 +28,52 @@ def atm_diff_flux(
     return k_md * (cw_mmolm3 - csat_mmolm3)
 
 
-def hcp(
+def henry_coeff(
     varname: str,
-    temp_c: pd.Series,
-    sal_psu: pd.Series,
-    catm_ppm: pd.Series,
-    constant: dict,
-    coeff: int = 1,
-) -> pd.Series:
+    temp_c: np.ndarray | float,
+    salt_inc: bool = False,
+    sal_psu: np.ndarray | float = 0.0,
+    catm_ppm: np.ndarray | float = 0.0,
+) -> np.ndarray | float:
     """Calculate Henry's Coefficient
 
     Parameters
     ----------
-    varname : string
-        Could be ch4 or co2
-    temp_c : pandas.Series
-        Temperature of equilibrium.
-    sal_psu : pandas.Series
-        Water salinity in PSU
-    constant : dict
-        Constant data (defined in utils/constant.yml).
-    coeff : float
-        0 Use Sanders 2015 - Only consider correction by temperature (Use this for freshwaters).
-        1 Use Wiss 1974 to estimate Henry's Coefficient for CO2 and Wiesenburg & Guinasso 1979 for CH4, considering Salinity and Temperature coerrections (default).
+    varname : Could be ch4 or co2
+    temp_c : Temperature of equilibrium in °C.
+    constant : Constant data (defined in utils/constant.yml).
+    salt_inc : bool
+    sal_psu : Water salinity in PSU
 
     Returns
     -------
-    hcp_t : float
-            Henry's coefficient for CH4 or CO2 in mmolm-3Pa-1
+    hcp_t : Henry's coefficient for CH4 or CO2 in mmolm-3Pa-1
     """
 
-    if coeff == 0:
-        if varname == "ch4":
-            hcp25 = constant["H_CH4_T25"]
-            dlnHcpd1_T = constant["dlnH_CH4_d1T"]
-        elif varname == "co2":
-            hcp25 = constant["H_CO2_T25"]
-            dlnHcpd1_T = constant["dlnH_CO2_d1T"]
-        hcp_t = hcp25 * np.exp(dlnHcpd1_T * (1 / (temp_c + 273.15) - 1 / 298.15))
-        return hcp_t
+    constant = read_constant()
+    if varname.upper() not in constant["GASES"]:
+        raise ValueError(f"{varname} is not on the permited list {constant['GASES']}")
 
-    elif coeff == 1:
-        hcp_t = hcp_sal(varname, sal_psu, temp_c, catm_ppm)
-        return hcp_t
+    if not salt_inc:
+        # Use Sanders 2015 - Only consider correction by temperature (Use this for freshwaters).
+        hcp_t = hcp_sanders(varname, temp_c)
     else:
-        ## CHANGE TO RAISE AN ERROR
-        return None
+        # Use Wiss 1974 to estimate Henry's Coefficient for CO2 and Wiesenburg & Guinasso 1979 for CH4, considering Salinity and Temperature coerrections.
+        hcp_t = hcp_sal(varname, sal_psu, temp_c, catm_ppm)
+    return hcp_t
+
+
+def hcp_sanders(varname: str, temp_c: np.ndarray | float) -> np.ndarray | float:
+    constant = read_constant()
+    hcp25 = constant[varname]["H_T25"]
+    dlnHcpd1_T = constant[varname]["dlnHdT"]
+    hcp_t = hcp25 * np.exp(dlnHcpd1_T * (1 / (temp_c + 273.15) - 1 / 298.15))
+    return hcp_t
 
 
 def hcp_sal(
-    var: str, sal_psu: pd.Series, temp_c: pd.Series, catm_ppm: pd.Series
-) -> pd.Series:
+    var: str, sal_psu: np.ndarray | float, temp_c: np.ndarray | float, catm_ppm: np.ndarray | float
+) -> np.ndarray | float:
     """Correct Henrys coefficient for CO2 (Weiss 1974) and CH4 (Wiesenburg and Guinasso 1979) considering salnity and temperature corrections.
 
     Parameters
@@ -91,8 +90,12 @@ def hcp_sal(
     hcpsalt_mmolm3Pa : list
         Henry's coefficient estimated considering salinity and temperature corrections [molm-3Pa-1]
     """
+    import henry_coeff
+
     temp_k = temp_c + 273.15
     fx = catm_ppm * 1e-6
+    constant = read_constant()
+
     if var == "co2":  # folowing Weiss 1974
         A = [-58.0931, 90.5069, 22.2940]
         B = [0.027766, -0.025888, 0.0050578]
@@ -107,22 +110,7 @@ def hcp_sal(
             / 101325
         )
     elif var == "ch4":  # folowing Wiesenburg and Guinasso 1979
-        # A = [-417.5053, 599.8626, 380.3636, -62.0764]
-        A = [-415.2807, 596.8104, 379.2599, -62.0757]
-        B = [-0.05916, 0.032174, -0.0048198]
 
-        c_molm3 = (
-            np.exp(
-                +np.log(fx)
-                + A[0]
-                + A[1] * 100 / temp_k
-                + A[2] * np.log(temp_k / 100)
-                + A[3] * (temp_k / 100)
-                + sal_psu * (B[0] + B[1] * temp_k / 100 + B[2] * (temp_k / 100) ** 2)
-            )
-            * 1000
-            / 1e9
-        )
-        hcpsalt_molm3Pa = c_molm3 / (101325 * fx)
-    return hcpsalt_molm3Pa
 
+if __name__ == "__main__":
+    print(henry_coeff("CH4", np.array(25.0)))
